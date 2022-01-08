f44852cafb68ec427d38ceca12c741753ac2ea8e
Daniel Brož <danobroz66@gmail.com>
2022-01-08 15:40:58 +0100

    initial music player setup

---
+++ js/data/songs.js
@@ -0,0 +1,50 @@
+export const songsList = [{
+    "id": "1",
+    "url": "../../music/3am.mp3",
+    "time": "03:36",
+    "title": "3 AM",
+    "artist": "Chiodos"
+  },
+  {
+    "id": "2",
+    "url": "../../music/falling-apart.mp3",
+    "time": "03:10",
+    "title": "Falling Apart",
+    "artist": "Zebra Head"
+  },
+  {
+    "id": "3",
+    "url": "../../music/little-lie.mp3",
+    "time": "03:16",
+    "title": "Little lie",
+    "artist": "Simple Creatures"
+  },
+  {
+    "id": "4",
+    "url": "../../music/mindset.mp3",
+    "time": "03:37",
+    "title": "Mindset",
+    "artist": "Every Avenue"
+  },
+  {
+    "id": "5",
+    "url": "../../music/nikki.mp3",
+    "time": "04:16",
+    "title": "Nikki",
+    "artist": "Forever The Sickest kids"
+  },
+  {
+    "id": "6",
+    "url": "../../music/paperthin-hymn.mp3",
+    "time": "03:19",
+    "title": "Paperthin hymn",
+    "artist": "Anberlin"
+  },
+  {
+    "id": "7",
+    "url": "../../music/until-the-end.mp3",
+    "time": "03:57",
+    "title": "Until the end",
+    "artist": "Quiet drive"
+  },
+];
